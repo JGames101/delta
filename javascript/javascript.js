e632277dd064301189afed9990f45babdae2a71c
JGames101 <jbmadden@hotmail.ca>
2017-08-06 06:42:57 -0700

    New JavaScript Popup

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -16,3 +16,19 @@ function conf(p) {
 		var useless = "cool cool";
 	}
 }
+function notifyMe() {
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+    var notification = new Notification('Notification title', {
+      icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+      body: "Hey there! You've been notified!",
+    });
+
+    notification.onclick = function () {
+      window.open("http://stackoverflow.com/a/13328397/1269037");      
+    };
+
+  }
+
+}
