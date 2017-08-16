591b57f026c25abefd2bf7e38e8bda348fb501f9
JGames101 <jbmadden@hotmail.ca>
2017-08-16 16:13:01 -0700

    New Offline Support

--- sw.js
+++ sw.js
@@ -1,34 +1,18 @@
 // we'll version our cache (and learn how to delete caches in
 // some other post)
-const cacheName = 'v1::static';
+const cacheName = 'v2::static';
 
-self.addEventListener('install', e => {
-  // once the SW is installed, go ahead and fetch the resources
-  // to make this work offline
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        '/',
-        /*
-          DEAR READER,
-          ADD A LIST OF YOUR ASSETS THAT
-          YOU WANT TO WORK WHEN OFFLINE
-          TO THIS ARRAY OF URLS
-        */
-      ]).then(() => self.skipWaiting());
-    })
-  );
-});
-
-// when the browser fetches a url, either response with
-// the cached object or go ahead and fetch the actual url
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    // ensure we check the *right* cache to match against
-    caches.open(cacheName).then(cache => {
-      return cache.match(event.request).then(res => {
-        return res || fetch(event.request)
-      });
-    })
-  );
+// Event listener for retrieving data
+self.addEventListener('fetch', function(event) {
+    var req = event.request.clone();
+    if (req.clone().method == "GET") {
+        event.respondWith(  
+         
+	    // Get the response from the network
+	    return fetch(req.clone()).then(function(response) {
+	       // And store it in the cache for later
+		return cache.put(req.clone(), response);
+	    });	
+        );
+    }
 });
