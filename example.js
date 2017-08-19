31e51692354150a53fc49e272d156abf7926a6ca
JGames101 <jbmadden@hotmail.ca>
2017-08-19 15:47:09 -0700

    Update example.js

--- example.js
+++ example.js
@@ -1,1 +1,19 @@
+new (function() {
+	var ext = this;
+	var descriptor = {
+		blocks: [
+			// Here is where you create blocks.
+		],
+		url : '' // This is the URL for when someone clicks "learn more"
+	};
+	ext._shutdown = function() {
+		
+	};
+	
+	ext._getStatus = function() {
+		return {status:2, msg:'Ready'}; // Returning status defines the colour of the light beside the blocks.
+	};
 
+	
+	ScratchExtensions.register('NAME', descriptor, ext); // Name of the extension.
+})();
