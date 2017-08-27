2eb1de9beb5f4a9542e921ae87716486238e4ae7
JGames101 <jbmadden@hotmail.ca>
2017-08-27 10:03:35 -0700

    Disabled javascript hiding the JS off alert for testing

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
-document.getElementById('jsmissing').style.display = 'none';
+document.getElementById('jsmissing').style.display = 'block';
 var online = navigator.onLine;
 if (online == true) {
     document.getElementById('connecmissing').style.display = 'none';
