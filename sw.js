e64838076804a2d0de4515044c6fe2fc9913f237
JGames101 <jbmadden@hotmail.ca>
2017-08-26 18:52:55 -0700

    Service worker should now be fully functional

--- sw.js
+++ sw.js
@@ -1,26 +1,40 @@
-// my custom service worker to get offline support working
-var online = navigator.onLine;
-var CACHE_VERSION = 1;
-if (online == true)
-  event.waitUntil(
-    caches.open(CACHE_VERSION).then(function(cache) {
+// we'll version our cache (and learn how to delete caches in
+// some other post)
+const cacheName = 'v3::static';
+
+self.addEventListener('install', e => {
+  // once the SW is installed, go ahead and fetch the resources
+  // to make this work offline
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
       return cache.addAll([
-        '',
-        '/angular/',
+        '/',
         '/javascript/',
-        '/html5/',
-        '/styles.css',
-        '/photos/flowers.jpg',
-        '/photos/',
-        '/photos/images.css',
-        '/photos/cactus.jpg',
-        '/photos/dandelion.jpg',
+		'/html5/',
+		'/photos/',
+		'/angular/',
+		'/updates/',
+		'/photos/cactus.jpg',
+		'/photos/dandelion.jpg',
+		'/photos/flowers.jpg',
 		'/photos/snapdragon.jpg',
+		'/styles.css',
 		'/global.js',
-		'jsmissing.html'
-      ]);
+		'/jsmissing.html',
+      ]).then(() => self.skipWaiting());
     })
   );
-else {
-	
-}
+});
+
+// when the browser fetches a url, either response with
+// the cached object or go ahead and fetch the actual url
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    // ensure we check the *right* cache to match against
+    caches.open(cacheName).then(cache => {
+      return cache.match(event.request).then(res => {
+        return res || fetch(event.request)
+      });
+    })
+  );
+});
