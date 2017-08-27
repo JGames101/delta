64a47ecf3d6be31e88ad64b1fc5b87e8baeb08f6
JGames101 <jbmadden@hotmail.ca>
2017-08-26 19:19:54 -0700

    New service worker function to load from URL if online

--- sw.js
+++ sw.js
@@ -1,14 +1,7 @@
 // we'll version our cache (and learn how to delete caches in
 // some other post)
-const cacheName = 'v3::static';
-var online = navigator.onLine;
-if (online == true)
-	window.location.reload(true);
-	document.getElementById('offlinealert').style.display = 'none';
-
+const cacheName = 'v4::static';
 self.addEventListener('install', e => {
-  // once the SW is installed, go ahead and fetch the resources
-  // to make this work offline
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
@@ -34,12 +27,17 @@ self.addEventListener('install', e => {
 // when the browser fetches a url, either response with
 // the cached object or go ahead and fetch the actual url
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    // ensure we check the *right* cache to match against
+if (online == true) {
+    event.respondWith(
+		fetch('https://jgames101.github.io/')
+	);
+} else { 
+    event.respondWith(
     caches.open(cacheName).then(cache => {
       return cache.match(event.request).then(res => {
         return res || fetch(event.request)
       });
-    })
-  );
+	})
+	);
+}
 });
