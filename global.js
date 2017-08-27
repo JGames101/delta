9f891a85a9663f5909a266570bdde3823f890cef
JGames101 <jbmadden@hotmail.ca>
2017-08-26 18:57:19 -0700

    hopefully fixing caching issue

--- global.js
+++ global.js
@@ -1,6 +1,7 @@
 document.getElementById('jsmissing').style.display = 'none';
 var online = navigator.onLine;
 if (online == true)
+	window.location.reload(true);
 	document.getElementById('offlinealert').style.display = 'none';
 else {
 	console.log('Offline! Element of offline users being shown...');
