860f53786154cecbc3305b40921ae739989e4aab
JGames101 <jbmadden@hotmail.ca>
2017-08-27 10:11:11 -0700

    "JS Missing" and offline alerts now appear

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
-document.getElementById('jsmissing').style.display = 'block';
+document.getElementById('jsmissing').style.display = 'none';
 var online = navigator.onLine;
 if (online == true) {
     document.getElementById('connecmissing').style.display = 'none';
