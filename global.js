4621be361aa7b3a247a16ab11e971731cbb45fb8
JGames101 <jbmadden@hotmail.ca>
2017-08-27 12:21:54 -0700

    Navigation menu is now partly functional

--- global.js
+++ global.js
@@ -1,7 +1,18 @@
 document.getElementById('jsmissing').style.display = 'none';
+var menu = false;
 var online = navigator.onLine;
 if (online == true) {
     document.getElementById('connecmissing').style.display = 'none';
 } else {
     document.getElementById('connecmissing').style.display = 'block';
+}
+
+function toggleMenu() {
+	if (menu == true) {
+		document.getElementById('menu').style.display = 'none';
+		menu = false;
+	} else {
+		document.getElementById('connecmissing').style.display = 'block';
+		menu = true;
+	}
 }
