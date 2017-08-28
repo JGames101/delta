cd9636606c005df064faf0217b92b2381f07a776
JGames101 <jbmadden@hotmail.ca>
2017-08-27 18:35:41 -0700

    Add files via upload

--- global.js
+++ global.js
@@ -1,6 +1,5 @@
 document.getElementById('jsmissing').style.display = 'none';
 document.getElementById('menuButton').style.display = 'block';
-var imgNum = 1;
 var menu = false;
 var online = navigator.onLine;
 if (online == true) {
@@ -20,18 +19,4 @@ function toggleMenu() {
 }
 function setPage(newLocation) {
 	window.location.href = "https://jgames101.github.io" + newLocation;
-}
-function nextImg() {
-	imgNum = imgNum + 1;
-	if (imgNum = 5) {
-		imgNum = 0;
-	}
-	document.getElementById("photo").src = "imgNum" + .jpg;
-}
-function prevImg() {
-	imgNum = imgNum - 1;
-	if (imgNum = 0) {
-		imgNum = 4;
-	}
-	document.getElementById("photo").src = "imgNum" + .jpg;
 }
