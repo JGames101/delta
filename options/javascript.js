f3560565a57d5935f12d387509d3c922c74f5fcb
JGames101 <jbmadden@hotmail.ca>
2017-09-01 21:14:55 -0700

    subpages: v1.04!

---
+++ options/javascript.js
@@ -0,0 +1,21 @@
+if (localStorage.getItem("backgroundImage") == undefined) {
+	localStorage.setItem("backgroundImage", "3");
+}
+
+document.getElementById('currentTheme').innerHTML = localStorage.getItem("themeColour");
+document.getElementById('newColour').value = localStorage.getItem("themeColour");
+
+
+function setBackground(backNum) {
+	localStorage.setItem("backgroundImage", backNum);
+	console.log(localStorage.getItem("backgroundImage"));
+	document.body.style.backgroundImage='url(/photos/' + localStorage.getItem("backgroundImage") + '.jpg)';
+}
+
+function setColour() {
+	var newColour = document.getElementById('newColour').value;
+	localStorage.setItem("themeColour", newColour);
+	console.log(localStorage.getItem("themeColour"));
+	document.getElementById('currentTheme').innerHTML = localStorage.getItem("themeColour");
+	document.getElementById('menu').style.backgroundColor = localStorage.getItem("themeColour");
+}
