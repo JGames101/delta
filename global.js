cf9e2d892c5c21059281792daf1847c615c9f1ea
JGames101 <jbmadden@hotmail.ca>
2017-09-03 09:14:03 -0700

    Removed Connecmissing

--- global.js
+++ global.js
@@ -1,10 +1,5 @@
 var menu = false;
 var online = navigator.onLine;
-if (online == true) {
-    document.getElementById('connecmissing').style.display = 'none';
-} else {
-    document.getElementById('connecmissing').style.display = 'block';
-};
 if (localStorage.getItem("backgroundImage") == undefined) {
 	console.log("Missing background image settings.");
 } else {
