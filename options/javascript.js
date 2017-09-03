97996f36748dbe6e72fe00e939143b8f4d48a153
JGames101 <jbmadden@hotmail.ca>
2017-09-02 21:56:28 -0700

    Suggested Colours

--- options/javascript.js
+++ options/javascript.js
@@ -2,15 +2,18 @@ if (localStorage.getItem("backgroundImage") == undefined) {
 	localStorage.setItem("backgroundImage", "3");
 }
 
+var suggestedColours = ["#ddad08", "#b4697d", "#e01453", "#d2cc2b", "#718181", "#acd56f", "#cb2c36", "#8098ae"];
 document.getElementById('currentTheme').innerHTML = localStorage.getItem("themeColour");
 document.getElementById('newColour').value = localStorage.getItem("themeColour");
 document.getElementById('colourPicker').value = localStorage.getItem("themeColour");
+document.getElementById('suggestedPreview').style.color = suggestedColours[localStorage.getItem("backgroundImage") - 1];
 
 
 function setBackground(backNum) {
 	localStorage.setItem("backgroundImage", backNum);
 	console.log(localStorage.getItem("backgroundImage"));
 	document.body.style.backgroundImage='url(/photos/' + localStorage.getItem("backgroundImage") + '.jpg)';
+	document.getElementById('suggestedPreview').style.color = suggestedColours[localStorage.getItem("backgroundImage") - 1];
 }
 
 function setColour() {
@@ -35,6 +38,14 @@ function setPickerColour() {
 
 function resetColour() {
 	localStorage.setItem("themeColour", "#486e89");
+	document.getElementById('currentTheme').innerHTML = localStorage.getItem("themeColour");
+	document.getElementById('menu').style.backgroundColor = localStorage.getItem("themeColour");
+	document.getElementById('newColour').value = localStorage.getItem("themeColour");
+	document.getElementById('colourPicker').value = localStorage.getItem("themeColour");
+}
+
+function setSuggestedColour() {
+	localStorage.setItem("themeColour", suggestedColours[localStorage.getItem("backgroundImage") - 1]);
 	document.getElementById('currentTheme').innerHTML = localStorage.getItem("themeColour");
 	document.getElementById('menu').style.backgroundColor = localStorage.getItem("themeColour");
 	document.getElementById('newColour').value = localStorage.getItem("themeColour");
