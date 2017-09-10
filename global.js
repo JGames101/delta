1a697545a7a73ecaae0877646f8438c0ff2d463e
JGames101 <jbmadden@hotmail.ca>
2017-09-09 17:01:31 -0700

    v1.2b

--- global.js
+++ global.js
@@ -11,16 +11,22 @@ if (localStorage.getItem("themeColour") == undefined) {
 $('head').append('<meta name="theme-color" content="' + localStorage.getItem("themeColour") + '" />');
 document.getElementById('menu').style.backgroundColor = localStorage.getItem("themeColour");
 
-function toggleMenu() {
+function toggleMenu() { //The commented code is for the page responding to when the menu is opened.
 	if (menu == true) {
 		document.getElementById('menuTextBox').style.color = 'transparent';
 		document.getElementById('menu').className = 'hidden';
 		document.getElementById('menuTextBox').style.cursor = "initial";
+		document.getElementById('menuToggle').innerHTML = 'keyboard_arrow_right';
+		//document.getElementsByClassName('content')[0].id = 'initial';
+		//document.getElementById('pageName').className = 'hidden';
 		menu = false;
 	} else {
 		document.getElementById('menuTextBox').style.color = 'white';
 		document.getElementById('menu').className = 'open';
 		document.getElementById('menuTextBox').style.cursor = "pointer";
+		document.getElementById('menuToggle').innerHTML = 'keyboard_arrow_left';
+		//document.getElementsByClassName('content')[0].id = 'contentOpen';
+		//document.getElementById('pageName').className = 'pageNameCondensed';
 		menu = true;
 	}
 }
@@ -30,4 +36,65 @@ function setPage(newLocation) {
 
 function simulateOverlay() {
 	document.getElementById('jsmissing').style.display = 'block';
+}
+
+function getAllUrlParams(url) {
+  // get query string from url (optional) or window
+  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+  // we'll store the parameters here
+  var obj = {};
+
+  // if query string exists
+  if (queryString) {
+
+    // stuff after # is not part of query string, so get rid of it
+    queryString = queryString.split('#')[0];
+
+    // split our query string into its component parts
+    var arr = queryString.split('&');
+
+    for (var i=0; i<arr.length; i++) {
+      // separate the keys and the values
+      var a = arr[i].split('=');
+
+      // in case params look like: list[]=thing1&list[]=thing2
+      var paramNum = undefined;
+      var paramName = a[0].replace(/\[\d*\]/, function(v) {
+        paramNum = v.slice(1,-1);
+        return '';
+      });
+
+      // set parameter value (use 'true' if empty)
+      var paramValue = typeof(a[1])==='undefined' ? true : a[1];
+
+      // (optional) keep case consistent
+      paramName = paramName.toLowerCase();
+      paramValue = paramValue.toLowerCase();
+
+      // if parameter name already exists
+      if (obj[paramName]) {
+        // convert value to array (if still string)
+        if (typeof obj[paramName] === 'string') {
+          obj[paramName] = [obj[paramName]];
+        }
+        // if no array index number specified...
+        if (typeof paramNum === 'undefined') {
+          // put the value on the end of the array
+          obj[paramName].push(paramValue);
+        }
+        // if array index number specified...
+        else {
+          // put the value at that index number
+          obj[paramName][paramNum] = paramValue;
+        }
+      }
+      // if param name doesn't exist yet, set it
+      else {
+        obj[paramName] = paramValue;
+      }
+    }
+  }
+
+  return obj;
 }
