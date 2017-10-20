47ed6a5e6128aa70899d7833fa8746e28b815d93
JGames101 <jbmadden@hotmail.ca>
2017-10-20 10:44:30 -0700

    MORE speed improvements

--- page/James M.js
+++ page/James M.js
@@ -7,4 +7,3 @@ xmlhttp.onreadystatechange = function() {
 };
 xmlhttp.open("GET", "/latest.json", true);
 xmlhttp.send();
-document.getElementById('pinned3').innerHTML = '<h2 class="pinned">OTHER</h2><div class="card wideCard card3"><iframe class="video" width="100%" height="315px" src="https://www.youtube.com/embed/z5XhoUH3sEc" frameborder="0" allowfullscreen></iframe><h2>BERLIN VR</h2><p>Some VR images from Berlin.</p></div>'
