995ef90d78f13c80e20dba08a065ee520badc491
JGames101 <jbmadden@hotmail.ca>
2017-11-03 17:10:55 -0700

    Version 2.1

--- setup/script.js
+++ setup/script.js
@@ -11,7 +11,7 @@ function setTheme(themeId) {
 	}
 };
 function setPhoto(photoId) {
-	var suggestedColours = ["#ddad08", "#b4697d", "#e01453", "#d2cc2b", "#718181", "#acd56f", "#cb2c36", "#8098ae"];
+	var suggestedColours = ["#ddad08", "#b4697d", "#e01453", "#d2cc2b", "#718181", "#acd56f", "#d32f2f", "#8098ae"];
 	localStorage.setItem("backgroundImage", photoId);
 	localStorage.setItem("themeColour", suggestedColours[photoId - 1]);
 	$(".photoChoice").css("color", "black");
@@ -22,4 +22,7 @@ function setPhoto(photoId) {
 function finish() {
 	localStorage.setItem("user", 'true');
 	window.location.href = "/";
+};
+function setColour(colour) {
+	localStorage.setItem("themeColour", colour);
 };
